cb8ab2ef5ed84479eb434a586245e5964c218e61
Micheal Makhoul(UST <236351@ust-global.com>
2022-11-20 18:34:04 +1100

    last version

--- src/group6/Util.java
+++ src/group6/Util.java
@@ -7,32 +7,55 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
-* The Util Class contains a number of methods to simplify the functionality of
-* other classes and ensures better re-usability.
-* 
-* @author Group-6
-*/
+ * The Util Class contains a number of methods to simplify the functionality of
+ * other classes and ensures better re-usability.
+ *
+ * @author Group-6
+ */
 public class Util {
+
     /**
      * Regex formats for email and password.
      */
-    static final String emailRegex = ".*\\..*@university.com";
-    static final String passwordRegex = "^[A-Z][a-z]{5,}[0-9]{3,}$";
- 
+    public static final String emailRegex = ".*\\..*@university.com";
+    public static final String passwordRegex = "^[A-Z][a-z]{5,}[0-9]{3,}$";
+
+    /**
+     * A group of colours - used to change the output when the program displays
+     * data to STDOUT.
+     */
+    public static final String RED_BOLD = "\033[1;31m";    // RED
+    public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
+    public static final String WHITE_BOLD = "\033[0m";  // WHITE
+
     /**
      * Read integer value.
+     *
      * @param prompt - String prompt
      * @return users input as integer
      */
     public static int readNumber(String prompt) {
         System.out.print(prompt);
-        return In.nextInt();
+        String number;
+
+        while (!(number = In.nextLine()).chars().allMatch(Character::isDigit) || number.isEmpty()) {
+
+            int tabs = prompt.lastIndexOf('\t');
+            String indent = prompt.substring(0, tabs + 1);
+
+            System.out.printf(RED_BOLD + indent + "%s %n" + WHITE_BOLD + "%s", "Invalid input", prompt);
+        }
+
+        return Integer.valueOf(number);
     }
 
     /**
      * Read String value.
+     *
      * @param prompt - String prompt
-     * @return users input as string 
+     * @return users input as string
      */
     public static String readString(String prompt) {
         System.out.print(prompt);
@@ -41,6 +64,7 @@ public class Util {
 
     /**
      * Generates random numbers between two numbers.
+     *
      * @param min - the lower boned of randomization.
      * @param max - the upper boned of randomization.
      * @return random integer.
@@ -48,84 +72,86 @@ public class Util {
     public static int generatRand(int min, int max) {
         return new Random().ints(min, max + 1).findAny().getAsInt();
     }
-    
+
     /**
      * Determines the grade based on the mark
+     *
      * @return grade
      */
-    public static String grade(double mark){
-        return  mark >= 85 ? "HD":
-                mark >= 75 ? "D" :
-                mark >= 65 ? "C" :
-                mark >= 50 ? "P" : "Z";
+    public static String grade(double mark) {
+        return mark >= 85 ? "HD"
+                : mark >= 75 ? "D"
+                        : mark >= 65 ? "C"
+                                : mark >= 50 ? "P" : "Z";
     }
 
     /**
      * Checks if the input matches the correct format as specified in regex.
+     *
      * @param input - String input
-     * @param regex - regex 
+     * @param regex - regex
      * @return true or false
      */
     public static boolean checkRegex(String input, String regex) {
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(input);
-        
+
         return m.find();
     }
-    
+
     /**
      * Checks if the email satisfies the required regex format.
+     *
      * @param email - Students email
      * @return true or false
      */
-    public static boolean emailRegex(String email){
+    public static boolean emailRegex(String email) {
         return Util.checkRegex(email, emailRegex);
     }
-    
+
     /**
      * Checks if the password satisfies the required regex format.
+     *
      * @param password - Students password
      * @return true or false
      */
-    public static boolean passwordRegex(String password){
+    public static boolean passwordRegex(String password) {
         return checkRegex(password, passwordRegex);
     }
-    
+
     /**
-     * Returns the length of the longest name in the list of students 
-     * who has enrolled in one subject at least - Used to align the output.
+     * Returns the length of the longest name in the list of students who has
+     * enrolled in one subject at least - Used to align the output.
+     *
      * @param list - list of student objects
      * @return length of the longest student name in the list.
      */
-    public static int maxNameLength(List<Student> list){
+    public static int maxNameLength(List<Student> list) {
         return list.stream().filter(Student::hasEnrolled).mapToInt(s -> s.getName().length()).max().getAsInt();
     }
-    
+
     /**
-     * Returns the length of the longest name in the list of students - Used to align the output.
+     * Returns the length of the longest name in the list of students - Used to
+     * align the output.
+     *
      * @return length of the longest student name of all students.
      */
-    public static int maxNameLength(){
+    public static int maxNameLength() {
         List<Student> students = new Database().readStudents();
         return students.stream().mapToInt(s -> s.getName().length()).max().getAsInt();
     }
 
     /**
      * Updates students.data file after modifying the list.
+     *
      * @param list - list of student objects
      */
-    public static void updateFile(List<Student> list){
-            Database db = new Database();
+    public static void updateFile(List<Student> list) {
+        Database db = new Database();
         try {
             db.save(list);
         } catch (IOException ex) {
-            System.out.println(Util.RED_BOLD+"Unable to save data to students.data file"+Util.WHITE_BOLD);
+            System.out.println(RED_BOLD + "Unable to save data to students.data file" + WHITE_BOLD);
         }
     }
-    
-    public static final String RED_BOLD = "\033[1;31m";    // RED
-    public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
-    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
-    public static final String WHITE_BOLD = "\033[0m";  // WHITE
 }
